045d5da47607e2e0f782c5d694d5cbe3e7b886b9
Vuong <vuongtechnique2000@gmail.com>
2020-09-09 21:52:28 +0700

    complete the reveal on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,8 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll ($(".testimonial"),"75%");
+new RevealOnScroll ($(".feature-item"),"85%");
+
---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,31 @@
+import $ from "jquery";
+import Waypoints from './../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class RevealOnScroll {
+  constructor(els, offset) {
+    this.itemsToReveal = els;
+    this.offsetPercent = offset;
+    this.hideInitially();
+    this.createWayPoints();
+  }
+
+  hideInitially() {
+    this.itemsToReveal.addClass("reveal-item");
+  }
+
+  createWayPoints() {
+      var that= this;
+    this.itemsToReveal.each(function () {
+      var currentItem = this;
+      new Waypoint ({
+        element: currentItem,
+        handler: function() {
+            $(currentItem).addClass("reveal-item--is-visible");
+        },
+        offset: that.offsetPercent,
+      });
+    });
+  }
+}
+
+export default RevealOnScroll;
